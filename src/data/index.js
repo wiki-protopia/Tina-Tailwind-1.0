e5f67c1abd50ea34ac2cf0ddee7bf63fc921226f
wiki-protopia <wiki.protopia@gmail.com>
2024-03-11 09:43:29 -0700

    complete the navigation bar.

--- src/data/index.js
+++ src/data/index.js
@@ -6,46 +6,76 @@ import utilitytools from 'images/utilitytools.svg'
 import alerts from 'images/alerts.svg'
 import KYC from 'images/KYC.svg'
 import Docs from 'images/Docs.svg'
+import telegram from 'images/telegram.svg'
+import bird from 'images/bird.svg'
+import discord from 'images/discord.svg'
+import facehair from 'images/facehair.svg'
 
 export const mockdata = {
     sidesection: {
         sideitemdata: [
             {
+                id: 1,
                 svgsrc: presales,
                 title: "Presales",
                 submenu: true,
             },
             {
+                id: 2,
                 svgsrc: launchpad,
                 title: "LaunchPad",
                 submenu: true,
             },
             {
+                id: 3,
                 svgsrc: specialsales,
                 title: "Special Sales",
                 submenu: true,
             },
             {
+                id: 4,
                 svgsrc: lock,
                 title: "Lock",
                 submenu: true,
             },
             {
+                id: 5,
                 svgsrc: utilitytools,
                 title: "Utility & Tools",
                 submenu: true,
             },
             {
+                id: 6,
                 svgsrc: alerts,
                 title: "Presale Alerts",
             },
             {
+                id: 7,
                 svgsrc: KYC,
                 title: "KYC & Audits",
             },
             {
+                id: 8,
                 svgsrc: Docs,
                 title: "Docs",
+            },
+        ],
+        sideicondata: [
+            {
+                id: 1,
+                svgsrc: telegram,
+            },
+            {
+                id: 2,
+                svgsrc: bird,
+            },
+            {
+                id: 3,
+                svgsrc: discord,
+            },
+            {
+                id: 4,
+                svgsrc: facehair,
             },
         ],
     },
--- src/sections/navsection/index.js
+++ src/sections/navsection/index.js
@@ -6,7 +6,7 @@ const Navsection = () => {
             <div class="flex justify-between items-center">
                 <button class="text-gray-600 focus:outline-none">
                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                     </svg>
                 </button>
                 <span class="text-lg font-semibold">Welcome, Admin!</span>
