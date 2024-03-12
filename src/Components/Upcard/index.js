d2203b3ca38d5c41b1a147b2723727b83da6806e
wiki-protopia <wiki.protopia@gmail.com>
2024-03-12 10:32:39 -0700

    test correct color for upcard!

--- src/Components/Upcard/index.js
+++ src/Components/Upcard/index.js
@@ -15,7 +15,7 @@ const Upcard = ({ item }) => {
         </p>
         {item.submenu.map((sub) => (
           <div class="absolute flex bottom-0 left-0]" key={sub.id}>
-            <p class="text-[14px] tracking-normal text-left text-[#F20CEC] mt-[2px]">
+            <p class={`text-[14px] tracking-normal text-left text-[${sub.color}] mt-[2px]`}>
           {/* <div class={`absolute flex bottom-0 left-0 mb-[${25*(sub.id-1)}px]`} key={sub.id}>
             <p class={`text-[14px] tracking-normal text-left text-[${sub.color}] mt-[2px]`}> */}
               {sub.title}
